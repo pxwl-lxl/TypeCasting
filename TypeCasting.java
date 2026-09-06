aab054c0e162d97dfcc570d01dfdec70dbeb3a54
dumplinghua 🥟 <paulangelo.binbinon@gmail.com>
2026-09-06 08:38:31 +0800

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,23 @@
+public class TypeCasting {
+	public static void main(String[] args) {
+
+	// Type Casting - Changing one data type into another.
+	
+	// dataType double to int	
+
+	double price1 = 20.50;
+
+	int newPrice1 = (int) price1;
+
+	System.out.println(newPrice1);
+
+	// dataType int to double
+	
+	int price2 = 22;
+
+	double newPrice2 = (double) price2;
+
+	System.out.println(newPrice2);
+
+	}
+}
